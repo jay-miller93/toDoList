039b6eb00afe546813091af64b1534ca6e88d3f0
Jay <isaacsdaddy42293@gmail.com>
2017-07-25 15:09:21 -0400

    broke

---
+++ ToDoList/Models/List.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class List
+    {
+        public int ListID { get; set; }
+        public string Listtitle { get; set; }
+        public DateTime ListCreateDate { get; set; }
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
